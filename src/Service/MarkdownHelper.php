92e6fea76db54d4eebbe3c3f11df1523d0ead7bf
Georgi Valchev <gvalchev@neterra.net>
2021-12-27 08:57:01 +0200

    Added new markdown logging channel

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,28 +3,29 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
-    /**
-     * @var MarkdownInterface
-     */
-    private $markdown;
-    /**
-     * @var AdapterInterface
-     */
-    private $cache;
+    private MarkdownInterface $markdown;
+    private AdapterInterface $cache;
+    private LoggerInterface $logger;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache)
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $logger)
     {
         $this->markdown = $markdown;
         $this->cache = $cache;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
         $item = $this->cache->getItem('markdown_' . md5($source));
+
+        if (stripos($source, 'ipsum') !== false) {
+            $this->logger->info('They are talking about ipsum again!');
+        }
 
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
